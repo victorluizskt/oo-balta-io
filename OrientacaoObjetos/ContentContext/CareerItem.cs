7f9db2304ea48066c2577cde60a76d0e41bec3f9
Victor Luiz <victorluizcefe@gmail.com>
2025-01-26 19:24:53 -0300

    feat: interacting with lists

--- OrientacaoObjetos/ContentContext/CareerItem.cs
+++ OrientacaoObjetos/ContentContext/CareerItem.cs
@@ -1,8 +1,8 @@
 ﻿namespace OrientacaoObjetos.ContentContext;
-public class CareerItem
+public class CareerItem(int order, string title, string description, Course? course)
 {
-    public int Order { get; set; }
-    public string? Title { get; set; }
-    public string? Description { get; set; }
-    public Course? Course { get; set; }
+    public int Order { get; set; } = order;
+    public string? Title { get; set; } = title;
+    public string? Description { get; set; } = description;
+    public Course? Course { get; set; } = course;
 }
--- OrientacaoObjetos/Program.cs
+++ OrientacaoObjetos/Program.cs
@@ -13,3 +13,18 @@ foreach (var article in articles)
     Console.WriteLine(article.Title);
     Console.WriteLine(article.Url);
 }
+
+var courses = new List<Course>();
+var courseOOP = new Course("Fundamentos OOP", "fundamentos-oop");
+var courseCsharp = new Course("Fundamentos C#", "fundamentos-oop");
+var courseDotNet = new Course("Fundamentos .net", "fundamentos-oop");
+
+courses.Add(courseOOP);
+courses.Add(courseCsharp);
+courses.Add(courseDotNet);
+
+var careers = new List<Career>();
+var career = new Career("Especialista .NET", "especialista-dotnet");
+var careerItem = new CareerItem(1, "Comece por aqui", "", null);
+career.Careers.Add(careerItem);    
+careers.Add(career);
